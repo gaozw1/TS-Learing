ddccb8a5a01f57e03bc533bc5c3173893fa55549
gzw <iamgzw@foxmail.com>
2021-02-23 10:22:17 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,22 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+//装饰器
+function logClass(params) {
+    console.log(params);
+    //params就是当前类
+}
+//类装饰器作用：动态扩展类的属性和方法，在不修改类的前提下，扩展类的功能
+var HttpClient = /** @class */ (function () {
+    function HttpClient() {
+    }
+    HttpClient.prototype.getData = function () {
+    };
+    HttpClient = __decorate([
+        logClass
+    ], HttpClient);
+    return HttpClient;
+}());
